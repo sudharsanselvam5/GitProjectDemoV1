451a620b4acaa633e7d090a1ae460a65c8adc4d7
hp <051195ss@gmail.com>
2023-03-23 19:50:48 +0530

    First Commit....

---
+++ src/org/fourwheeler/FourWheeler.java
@@ -0,0 +1,26 @@
+package org.fourwheeler;
+
+
+
+public class FourWheeler {
+	public void car() {
+		System.out.println("The Name of Car is Toyata ");
+	}
+public void bus() {
+	System.out.println("The Name of bus is Benz ");
+	
+}
+public void lorry() {
+	System.out.println("The Name of lorry is Ashok Leyland");
+	
+}
+
+public static void main(String[] args) {
+	FourWheeler vehicle3 = new FourWheeler();
+	vehicle3.car();
+	vehicle3.bus();
+	vehicle3.lorry();
+}
+
+
+}
