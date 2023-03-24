8c8eb5612d63f18ef00d70cbbe78d3bba4185d7a
hp <051195ss@gmail.com>
2023-03-24 14:30:02 +0530

    commiting to BranchNew

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,33 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		System.out.println("Employee Id is : 1475");
+	}
+    private void empName() {
+System.out.println("Employee Name is : Ram");
+	}
+    private void empDob() {
+    	System.out.println("Employee DOB is : 01/01/2000");
+	}
+    private void empPhone() {
+    	System.out.println("Phone Number is:55655998874");
+    }
+    private void empEmail() {
+		System.out.println("Email is:456788@gmail.com");
+
+	}
+    private void empAddress() {
+	System.out.println("Address is chennai");
+
+	}
+    public static void main(String[] args) {
+		Employee data = new Employee();
+		data.empId();
+		data.empName();
+		data.empDob();
+		data.empPhone();
+		data.empEmail();
+		data.empAddress();
+	}
+}
